41315c6f7c79622a6442b45a42ce5bfc93805895
Ni3-y <nitinyewale7773@gmail.com>
2021-05-31 12:25:59 +0530

    third try

---
+++ test.java
@@ -0,0 +1,11 @@
+import pack1.*;
+import java.io.*;
+public class test
+{
+	public static void main(String args[])throws IOException
+	{
+		teacher p1=new teacher();
+		p1.accept();
+		p1.display();
+	}
+}
